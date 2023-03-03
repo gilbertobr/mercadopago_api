ae91ad477c75ef9f1a13187921860ec81c6d438d
Gilberto <gp_sj@hotmail.com>
2023-03-03 14:42:03 -0300

    Add Function Create Payment

--- lib/requests/payments.ex
+++ lib/requests/payments.ex
@@ -57,5 +57,62 @@ defmodule Mercadopago.Requests.Payments do
         Mercadopago.API.get("/v1/payments/#{payment_id}")
     end
 
+    @doc """
+    Create payment
+    [docs](https://www.mercadopago.com.br/developers/pt/reference/payments/_payments/post)    
+
+    ## Examples
+        iex> data =
+        %{
+          "additional_info": %{
+            "items": [
+              %{
+                "id": "MLB2907679857",
+                "title": "Point Mini",
+                "description": "Producto Point para cobros con tarjetas mediante bluetooth",
+                "picture_url": "https://http2.mlstatic.com/resources/frontend/statics/growth-sellers-landings/device-mlb-point-i_medium@2x.png",
+                "category_id": "electronics",
+                "quantity": 1,
+                "unit_price": 58.8
+              }
+            ],
+            "payer": %{
+              "first_name": "Test",
+              "last_name": "Test",
+              "phone": %{
+                "area_code": 11,
+                "number": "987654321"
+              },
+              "address": %{}
+            },
+            "shipments": %{
+              "receiver_address": %{
+                "zip_code": "12312-123",
+                "state_name": "Rio de Janeiro",
+                "city_name": "Buzios",
+                "street_name": "Av das Nacoes Unidas",
+                "street_number": 3003
+              }
+            },
+            "barcode": %{}
+          },
+          "description": "Payment for product",
+          "external_reference": "MP0001",
+          "installments": 1,
+          "metadata": %{},
+          "payer": %{
+            "entity_type": "individual",
+            "type": "customer",
+            "identification": %{}
+          },
+          "payment_method_id": "visa",
+          "transaction_amount": 58.8
+        }         
+        iex> Mercadopago.Requests.Payments.show(data)
+
+    """
+    def create(data) do
+        Mercadopago.API.post("/v1/payments", data)
+    end
 
 end
