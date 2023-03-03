2a963851a00fa6a8ef08219857b44b86745e0beb
Gilberto <gp_sj@hotmail.com>
2023-03-03 11:51:02 -0300

    Added Get Payment request

--- lib/requests/payments.ex
+++ lib/requests/payments.ex
@@ -10,6 +10,33 @@ defmodule Mercadopago.Requests.Payments do
     ## Examples
 
         iex> Mercadopago.Requests.Payments.search()
+        {:ok, [%{
+        date_created: "2023-02-10T19:45:48.000-04:00",
+        sponsor_id: nil,
+        money_release_status: "released",
+        status: "approved",
+        date_last_updated: "2023-02-10T19:47:23.000-04:00",
+        merchant_number: nil,
+        acquirer_reconciliation: [],
+        brand_id: nil,
+        captured: true,
+        order: %{},
+        authorization_code: nil,
+        date_of_expiration: nil,
+        ...
+        }]}    
+    """
+    def search() do
+        Mercadopago.API.get("/v1/payments/search")
+    end
+
+    @doc """
+    Get paid
+    [docs](https://www.mercadopago.com.br/developers/pt/reference/payments/_payments_id/get)    
+
+    ## Examples
+
+        iex> Mercadopago.Requests.Payments.show("123456789")
         {:ok, %{
         date_created: "2023-02-10T19:45:48.000-04:00",
         sponsor_id: nil,
@@ -26,8 +53,9 @@ defmodule Mercadopago.Requests.Payments do
         ...
         }}    
     """
-    def search() do
-        Mercadopago.API.get("/v1/payments/search")
+    def show(payment_id) do
+        Mercadopago.API.get("/v1/payments/#{payment_id}")
     end
+
 
 end
